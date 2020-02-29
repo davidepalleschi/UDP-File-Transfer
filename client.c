0bf7ff47629de83324f19342af413c4b42bf68dc
Lorenzo <lorenzo.zara96@gmail.com>
2020-02-29 17:17:23 +0100

    updated get command

--- client.c
+++ client.c
@@ -30,29 +30,37 @@
 #define PAYLOAD 1024
 #define TX_WINDOW 3
 #define TIMER 300000
+#define LOSS_PROBABILITY 0.15
 
 
 #define fflush(stdin) while(getchar()!='\n'){}
 
+/* nuovi tipi */
+typedef struct _packet_form{
+	int counter;
+	char buf[BUFFER_SIZE];
+	int ack;
+} packet_form;
+
 /* variabili globali */
+char buffer[BUFFER_SIZE];
+char **file_buffer;
 int num_pkt;
 int socket_fd, msg_rec, len; 
+int server_addr_len;
+struct sockaddr_in server_addr;
 
 /* funzioni */
 void receive_packets(void);
+int send_ack(int seq);
 
 
-int main() 
-{ 
-	struct sockaddr_in server_addr; 
-	char buffer[BUFFER_SIZE];
+int main() {  
 	char cmd[BUFFER_SIZE];
-	int server_addr_len;
 	int ret;	//variabile per il controllo dei return values
 	int filename_len;
 	char filename[NAME_LEN];
 	int file_len;
-	char **file_buffer;
 	int fd;
 
 	//Create Socket
@@ -212,10 +220,11 @@ get_insert:	// preparo il buffer in trasmissione
 			filename_len = strlen(cmd);
 			if (cmd[filename_len - 1] == '\n') cmd[filename_len - 1] = '\0';
 			strcpy(filename, cmd);
-
+sen:
 			// invio la richiesta di download del file
 			ret = sendto(socket_fd, cmd, sizeof(cmd), 0, (SA *) &server_addr, server_addr_len);
 			if (ret == -1){
+				if (errno == EINTR || errno == EAGAIN) goto sen;
 				printf("sendto() error while sending filename to be downloaded.\n");
 				exit(-1);
 			}
@@ -267,6 +276,18 @@ get_insert:	// preparo il buffer in trasmissione
 			}
 			
 			receive_packets();
+			printf("Packets received succesfully.\nCreating file...");
+			for (int i=0; i<num_pkt; i++){
+				ret = write(fd, file_buffer[i], PAYLOAD);
+				if (ret == -1){
+					printf("write() error on creating file from packets.\n");
+					exit(-1);
+				}
+			}
+			printf("File written succesfully.\n");
+			printf("Updating file list...\n");
+			//write file list
+
 
 			
 
@@ -316,12 +337,14 @@ get_insert:	// preparo il buffer in trasmissione
 
 void receive_packets(){
 
+	int ret; //variabile per il controllo dei return values
 	int pkts = 0;
 	int counter = 0;
-	int temp = 0;
-	int offset = 0;
+	int seq = 0;
 	int window = TX_WINDOW;
-	offset = num_pkt%window;
+	int offset = num_pkt%window;
+	packet_form packets[num_pkt];
+
 	struct timeval timer;
 	timer.tv_sec = 0;
 	timer.tv_usec = TIMER;
@@ -330,7 +353,85 @@ void receive_packets(){
 	while (pkts < num_pkt || counter <= num_pkt - 2){
 		if (TX_WINDOW % 2 == 1) window = offset;
 		else window = offset + 1;
+
+
+		// pacchetti ricevuti "window" alla volta
+		char pkt[BUFFER_SIZE];
+		char *RX_pkt;
+		for (int i=0; i<window; i++){
+	receive:
+			printf("");
+			RX_pkt = (char *) malloc(PAYLOAD * sizeof(char));
+			if (RX_pkt == NULL){
+				printf("malloc error.\n");
+				exit(-1);
+			}
+			bzero(pkt, BUFFER_SIZE);
+			bzero(RX_pkt, PAYLOAD);
+			ret = recvfrom(socket_fd, pkt, BUFFER_SIZE, 0, (SA *) &server_addr, &server_addr_len);
+			if (ret == -1){
+				if (errno == EAGAIN) goto catchemall;
+				else{
+					printf("recvfrom() error while receiving packets.\n");
+					exit(-1);
+				}
+			}
+			printf("");
+
+			// apprendimento della sequenza del pacchetto
+			seq = atoi(strtok(pkt, " "));
+			if (seq >= pkts) pkts = seq + 1;
+			printf("");
+			// ricezione del payload
+			char *c_index;
+			sprintf(c_index, "%d", seq);
+			int starting = strlen(c_index) + 1;
+			char *beginning = &pkt[starting];
+			char *end = &pkt[BUFFER_SIZE];
+			char *payload = (char *) malloc((end - beginning + 1) * sizeof(char)); // malloc((BUFFER_SIZE*seq - starting + 1) * sizeof(char));
+			if (payload == NULL){
+				printf("malloc error.\n");
+				exit(-1);
+			}
+			memcpy(payload, beginning, end - beginning);
+			RX_pkt = payload;
+			///SENDACK
+			if (send_ack(seq)){
+				if (strcpy(packets[seq].buf, RX_pkt) == NULL){
+					printf("strcpy error while receiving packet %s.\n", RX_pkt);
+					exit(-1);
+				}
+				if (strcpy(file_buffer[seq], packets[seq].buf) == NULL){
+					printf("strcpy error while receiving packet (%s, %s).\n", file_buffer[seq], packets[seq].buf);
+					exit(-1);
+				}
+			}
+	catchemall:
+			printf("");
+		}
 	}
+	return;
+}
+
+int send_ack(int seq){
+
+	int ret;
+	float loss_prob;
+	bzero(buffer, BUFFER_SIZE);
+	sprintf(buffer, "%d", seq);
+	
+	if (seq > num_pkt - TX_WINDOW - 1) loss_prob = 0;
+	else loss_prob = LOSS_PROBABILITY;
+
+	float rndm = (float) random() / RAND_MAX;
+	if (rndm < (1 - loss_prob)){
+		ret = sendto(socket_fd, buffer, 32, 0, (SA *) &server_addr, server_addr_len);
+		if (ret == -1){
+			printf("sendto() error while sending ack.\n");
+			exit(-1);
+		}
+		return 1;
+	}else return 0;
 }
 
 
