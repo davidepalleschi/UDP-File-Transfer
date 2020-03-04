9033123c943e62fe61f43605dec415d31c1e538b
Davide <davide9614@gmail.com>
2020-03-04 18:34:09 +0100

    put modified

--- server/src/cmd.c
+++ server/src/cmd.c
@@ -76,7 +76,7 @@ void cmd_recv_packets(char* file_name,char* file_size ,int socket_fd, struct soc
     printf("numero pacchetti: %d\n",num_pkt);
     printf("pacchetto %d: %d\n",num_pkt-1,packet[num_pkt-1].seq);
     int window=WINDOW;
-    if(num_pky<win){
+    if(num_pkt<window){
         window=num_pkt;
     }
     while(counter < num_pkt){
@@ -102,7 +102,7 @@ void cmd_recv_packets(char* file_name,char* file_size ,int socket_fd, struct soc
             packet[i].seq=atoi(seq);
             strcpy(packet[i].payload, buffer+64);
             fflush(stdout);
-            write(1,packet[i].payload);
+            write(1,packet[i].payload,PAYLOAD);
             sendto(socket_fd, seq, sizeof(seq) , 0, (SA *) &client_addr, len);
             if(packet[i].recv==1){
                 if(packet[i].seq==base){
